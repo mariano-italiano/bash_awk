ec545ee12053e1dc070c85e919f95ac02b70d2ff
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-11-15 16:26:53 +0100

    Update lab2.sh

--- solutions/lab2.sh
+++ solutions/lab2.sh
@@ -12,24 +12,45 @@ read -p "Podaj pierwszą liczbę: " a
 read -p "Podaj drugą liczbę: " b
 read -p "Podaj operację [+, -, /, *]: " operacja
 
-# Wykonanie operacji arytmetycznej i wyświetlenie wyniku
-echo -n "WYNIK: "
-case $operacja in
-        "+") echo "scale=2; $a+$b" | bc ;;
-        "-") echo "scale=2; $a-$b" | bc ;;
-        "/") echo "scale=2; $a/$b" | bc ;;
-        "*") echo "scale=2; $a*$b" | bc ;;
+if [ -z $a ] || [ -z $b ] ; then
+	echo "Jedna z liczb nie została podana"
+else
 
-        *)      echo "Wybrałeś niedozwolony znak operacji!" ;;
-esac
-
-# Pobranie czasu zakończenia skryptu
-koniec=`date +%s.%N`
-echo
-
-# Obliczenie długości trwania i wyświetlenie wartości na zółto
-czasWykonania=$( echo "$koniec - $start" | bc -l )
-echo -e "Czas wykonania obliczeń:\033[33m $czasWykonania \033[0msekund"
-echo
-
+	# Wykonanie operacji arytmetycznej i wyświetlenie wyniku
+	echo -n "WYNIK: "
+	case $operacja in
+	        "+") op_start=`date +%s.%N`
+		     echo "scale=2; $a+$b" | bc
+		     op_end=`date +%s.%N` ;;
+	        "-") op_start=`date +%s.%N`
+		     echo "scale=2; $a-$b" | bc 
+		     op_end=`date +%s.%N` ;;
+	        "/") if [ $b -gt 0 ] ; then
+			     op_start=`date +%s.%N`
+			     echo "scale=2; $a/$b" | bc 
+			     op_end=`date +%s.%N` 
+		     else
+		     	     echo "Nie dzielimy przez 0!"
+		             exit 1
+		     fi
+		     ;;
+	        "*") op_start=`date +%s.%N`
+		     echo "scale=2; $a*$b" | bc 
+		     op_end=`date +%s.%N` ;;
+	
+	        *)      echo "Wybrałeś niedozwolony znak operacji!" 
+			exit 1;;
+	esac
+	
+	# Pobranie czasu zakończenia skryptu
+	koniec=`date +%s.%N`
+	echo
+	
+	# Obliczenie długości trwania i wyświetlenie wartości na zółto
+	czasWykonania=$( echo "$koniec - $start" | bc -l )
+	czasObliczen=$( echo "$op_end - $op_start" | bc -l )
+	echo -e "Czas wykonania programu:\033[33m $czasWykonania \033[0msekund"
+	echo -e "Czas wykonania obliczeń:\033[33m $czasObliczen \033[0msekund"
+	echo
+fi
 exit 0
