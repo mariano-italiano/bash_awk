f46f5b1ff01a160c0aa16e73ba8df5720451b639
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-11-15 17:41:49 +0100

    Create lab1.sh

---
+++ solutions/lab1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Data: 01.01.2024
+# Autor: Marcin Kujawski
+# Opis: Skrypt wyświetlający "Cześć <imie>" dla uzytkownika
+# Użycie: ./lab1.sh <imie>
+
+
+if [ $# -eq 1 ] ; then
+        echo
+        echo "Cześć $1!"
+        echo
+else
+        echo
+        echo "Usage: $0 <imie>"
+        echo
+        exit 0
+fi
